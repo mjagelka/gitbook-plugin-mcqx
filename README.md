299cc7cc77ac5defe91885773e53ff55e9deb140
Yiu Man Chi <ymcatar@gmail.com>
2015-06-19 13:43:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+GitBook Quiz Plugin
+===
+
+This is a plugin for inserting multiple choice and fill-in-the-blanks questions in GitBook.
+
+Analytics features will be added later.
---
+++ index.js
@@ -0,0 +1,63 @@
+var jade = require('jade');
+
+var path = require('path');
+var fs = require('fs');
+
+var mcqCount;
+
+module.exports = {
+
+    // Extend ebook resources and html
+    website: {
+        assets: "./assets",
+        css: [
+            "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css",
+            "quizx.css"
+        ],
+        js: [
+            "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js",
+            "http://www.parsecdn.com/js/parse-1.4.2.min.js",
+            "quizx.js"
+        ]
+    },
+
+    // Extend templating blocks
+    blocks: {
+
+        mcq: {
+            blocks: ["title", "a", "b", "c", "d"],
+            process: function(blk) {
+                var mcqView = fs.readFileSync(path.resolve(__dirname, "./views/mcq.jade"));
+                var question = {id: '', ans:'', a:'', b:'', c:'', d:''};
+
+                question.id = blk.kwargs.id.trim();
+                question.ans = blk.kwargs.ans.trim();
+
+                if(question.target)
+                    question.target = blk.kwargs.target.trim();
+
+                blk.blocks.forEach(function(item){
+
+                    if(item.name == 'title')
+                        question.title = item.body.trim();
+                    else if(item.name == 'a')
+                        question.a = item.body.trim();
+                    else if(item.name == 'b')
+                        question.b = item.body.trim();
+                    else if(item.name == 'c')
+                        question.c = item.body.trim();
+                    else if(item.name == 'd')
+                        question.d = item.body.trim();
+                });
+
+                return jade.render(mcqView, {question: question, number: mcqCount++});
+            }
+        }
+    },
+
+    hooks: {
+
+        "init": function(){
+        }
+    }
+};
